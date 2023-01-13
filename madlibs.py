19bcb1959ef72b6a96fb00992c69d81bbd4bde33
PK <78058333+pkotak1794@users.noreply.github.com>
2023-01-13 10:40:49 -0800

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,29 @@
+# simple madlibs program by Priyanka. 
+
+def madlibs():
+    print("Welcome to Madlibs! Let's get started!")
+    noun = input("Please enter a noun: ")
+    place = input("Please enter a place: ")
+    adverb = input("Please enter an adverb: ")
+    adjective = input("Please enter an adjective: ")
+    noun_two = input("Please enter a noun: ")
+    adjective_two = input("Please enter an adjective: ")
+
+    print("-----------------------------------------")
+    print("There once was a ", noun, " who lived in a large castle.")
+    print("During the summer they would visit ", place, " and throw ", adjective, " parties.")
+    print("All the townsfolk, ", adverb, " attended but they never brought any ", noun_two, ".")
+    print("The ", noun, " got upset and decided not to invite the ", adjective_two, " townspeople to the parties anymore.")
+    print("Now the ", noun, " was lonely but it was better than hanging out with those freeloaders.")
+
+    return
+
+madlibs() # function call 
+
+print("Would you like to play again? (y/n): ")
+input("y/n: ")
+
+if "y":
+    madlibs() # runs madlibs function from the beginning
+else:
+    exit() # exits program 
